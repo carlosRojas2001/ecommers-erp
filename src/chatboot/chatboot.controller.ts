ee6de63b0e2ec8684fc3d9a0b17692f3cc483e40
carlosRojas2001 <crojas@hsperu.pe>
2026-08-21 17:55:06 -0500

    --

--- src/chatboot/chatboot.controller.ts
+++ src/chatboot/chatboot.controller.ts
@@ -1,26 +1,21 @@
-import { Controller, Post, Body, Query, Get, Req } from '@nestjs/common';
+import { Controller, Post, Body, Query, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { Throttle } from '@nestjs/throttler';
-import { Chat } from './chat.service';
-import { ChatbootService } from './chatboot.service';
-
+import { Chat } from './chat.service'; 
+ 
 @Controller('chatboot')
 export class ChatbootController {
   constructor(
     private readonly chatService: Chat,) {}
 
-  @Get()
-  async consult(@Query('search') search:string){
-    return this.chatService.filtrarTokensValidos([search])
+ @Get('ver-mas/:consultaId')
+  async consult(@Param('consultaId') consultaId: string, @Query('pagina', ParseIntPipe) pagina: number,){
+    return this.chatService.verMas(consultaId,pagina)
   }
 
   @Post()
-  @Throttle({ default: { limit: 10, ttl: 60000 } }) // 10 req/min por IP (previene abuso/costo del LLM)
-  async chat(
-    @Body('message')
-    message: string,
-    @Req() req: Request
-  ) {
-    return this.chatService.buscarArticulos(message, req);
+  @Throttle({ default: { limit: 30, ttl: 60000 } }) 
+  async chat( @Body('message') message: string, ) {
+    return this.chatService.buscarArticulos(message);
   }
 
 }
