5f52a4fdcac67c23165d1bc4538ecd7f7add8c6e
carlosRojas2001 <crojas@hsperu.pe>
2026-08-27 11:01:49 -0500

    --

--- src/chatboot/chatboot.controller.ts
+++ src/chatboot/chatboot.controller.ts
@@ -1,11 +1,12 @@
 import { Controller, Post, Body, Query, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { Throttle } from '@nestjs/throttler';
 import { Chat } from './chat.service'; 
+import { ChatbootService } from './chatboot.service';
  
 @Controller('chatboot')
 export class ChatbootController {
   constructor(
-    private readonly chatService: Chat,) {}
+    private readonly chatService: ChatbootService,) {}
 
  @Get('ver-mas/:consultaId')
   async consult(@Param('consultaId') consultaId: string, @Query('pagina', ParseIntPipe) pagina: number,){
@@ -15,7 +16,7 @@ export class ChatbootController {
   @Post()
   @Throttle({ default: { limit: 30, ttl: 60000 } }) 
   async chat( @Body('message') message: string, ) {
-    return this.chatService.buscarArticulos(message);
+    return this.chatService.chat(message);
   }
 
 }
--- src/chatboot/chatboot.module.ts
+++ src/chatboot/chatboot.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { ChatbootService } from './chatboot.service';
 import { ChatbootController } from './chatboot.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
-import { RedisModule } from './redis.module';
+// import { RedisModule } from './redis.module';
 import { Chat } from './chat.service';
 
 @Module({
-  imports: [PrismaModule, RedisModule],
+  imports: [PrismaModule],
   controllers: [ChatbootController],
   providers: [ChatbootService,Chat],
 })
