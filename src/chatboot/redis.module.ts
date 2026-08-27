d29c951969352a4b83c3c8309a662cb2d98a196f
carlosRojas2001 <crojas@hsperu.pe>
2026-08-27 10:36:02 -0500

    --

--- src/chatboot/redis.module.ts
+++ src/chatboot/redis.module.ts
@@ -1,27 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { RedisClientType } from 'redis';
 import { createClient } from 'redis';
 
 @Module({
   providers: [
     {
       provide: 'REDIS_CLIENT',
-      inject: [ConfigService],
       useFactory: async (configService: ConfigService) => {
-        const redisUrl =
-          configService.get<string>('REDIS_URL') ||
-          'redis://127.0.0.1:6380';
-
-        const client = createClient({
-          url: redisUrl,
-        });
-
-        client.on('error', (err) => {
-          console.error('Redis Client Error:', err);
-        });
-
+        
+        const client = createClient();
         await client.connect();
-
         return client;
       },
     },
